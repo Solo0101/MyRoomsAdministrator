aa5c8f3291d317ea5311962a5b14d88882707dab
Adrian Solomon <adriansolomon02@gmail.com>
2023-09-28 22:37:16 +0300

    routed Register button

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from "@angular/forms";
+import {Router} from "@angular/router";
 // import { websiteTitle } from "@/constants";
 
 @Component({
@@ -11,12 +12,24 @@ export class LoginComponent {
   hide: boolean = true;
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  constructor(private  router: Router) {
+
+  }
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  goToRegister($myParam: string = ''): void {
+    const navigationDetails: string[] = ['/sign-up'];
+    if($myParam.length) {
+      navigationDetails.push($myParam);
+    }
+    this.router.navigate(navigationDetails);
   }
 
   // protected readonly websiteTitle = websiteTitle;
